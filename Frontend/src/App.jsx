391708987849969d415d25f83a233931a139deec
DevashishMishra <devashishmishra39@gmail.com>
2025-02-07 20:39:10 +0530

    making frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './Component/Navbar'
+
+function App() {
+  
+
+  return (
+    <>
+    <Navbar/>
+    
+    </>
+  )
+}
+
+export default App
